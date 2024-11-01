a67119c0bd5086597f52c8fb225abd9e187bdda4
hharrari <hharrari@LABTEA19-4LMM>
2024-10-31 21:04:45 -0500

    done with requests 407 and 409

--- SingleResponsibilityPrinciple/RestfulTradeDataProvider.cs
+++ SingleResponsibilityPrinciple/RestfulTradeDataProvider.cs
@@ -1,21 +1,47 @@
-﻿using SingleResponsibilityPrinciple.Contracts;
+﻿using Azure;
+using SingleResponsibilityPrinciple.Contracts;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace SingleResponsibilityPrinciple
 {
     public class RestfulTradeDataProvider : ITradeDataProvider
     {
+        private readonly string _url;
+        private readonly ILogger _logger;
+        HttpClient client = new HttpClient();
         public RestfulTradeDataProvider(string url, ILogger logger)
         {
+            _url = url;
+            _logger = logger;
+        }
+        async Task<List<string>> GetTradeAsync()
+        {
+            _logger.LogInfo("Connecting to the Restful server using HTTP");
+            List<string> tradesString = null;
+
+            HttpResponseMessage response = await client.GetAsync(_url);
+            if (response.IsSuccessStatusCode)
+            {
+                string content = await response.Content.ReadAsStringAsync();
+                tradesString = JsonSerializer.Deserialize<List<string>>(content);
+                _logger.LogInfo("Received trade strings of length = " + tradesString.Count);
+            }
+            return tradesString;
         }
 
         public IEnumerable<string> GetTradeData()
         {
-            throw new NotImplementedException();
+            Task<List<string>> task = Task.Run(() => GetTradeAsync());
+            task.Wait();
+
+            List<string> tradeList = task.Result;
+            return tradeList;
         }
     }
+
 }
--- SingleResponsibilityPrinciple/URLTradeDataProvider.cs
+++ SingleResponsibilityPrinciple/URLTradeDataProvider.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,13 +10,44 @@ namespace SingleResponsibilityPrinciple
 {
     public class URLTradeDataProvider : ITradeDataProvider
     {
+        string url;
+        ILogger logger;
         public URLTradeDataProvider(string url, ILogger logger)
         {
+            this.url = url;
+            this.logger = logger;
         }
 
         public IEnumerable<string> GetTradeData()
         {
-            throw new NotImplementedException();
+            List<string> tradeData = new List<string>();
+            logger.LogInfo("Reading trades from URL: " + url);
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = client.GetAsync(url).Result;
+                if (!response.IsSuccessStatusCode)
+                {
+                    // log error and throw an exception if the URL fails
+                    logger.LogWarning($"Failed to retrieve data. Status code: {response.StatusCode}");
+                    throw new Exception($"Error retrieving data from URL: {url}");
+                }
+
+                // set up a Stream and StreamReader to access the data
+                using (Stream stream = response.Content.ReadAsStreamAsync().Result)
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    // Read each line of the text file using reader.ReadLine()
+                    // Read until the reader returns a null line
+                    // Add each line to the tradeData list
+                    String line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        tradeData.Add(line);
+                    }
+                }
+            }
+            return tradeData;
         }
     }
 }
